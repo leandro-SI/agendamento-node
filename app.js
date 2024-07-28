805c932753e703bb8a8773337bacdab0d7a95ca9
leandro-si <leandro.almeida@uvvnet.com.br>
2024-07-28 15:52:41 -0300

    estrutura inicial

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+mongoose.connect("mongodb://127.0.0.1:27017/agendamento").then(result => {
+    console.log('Banco conectado!!')
+}).catch(erro => console.log('Erro na conexão: ', erro))
+
+app.get('/', (request, response) => {
+    response.send('Teste')
+})
+
+app.listen(8080, () => {
+    console.log('Servidor iniciado!!')
+})
