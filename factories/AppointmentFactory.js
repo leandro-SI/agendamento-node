a216ce5ab9d07f33c9aa2cc6df85572b74399f43
leandro-si <leandro.almeida@uvvnet.com.br>
2024-07-30 22:17:02 -0300

    exibindo consultas no full calendar

--- factories/AppointmentFactory.js
+++ factories/AppointmentFactory.js
@@ -10,7 +10,6 @@ class AppointmentFactory {
         var minutes = Number.parseInt(simpleAppointment.time.split(':')[1]);
 
         var startDate = new Date(year, month, day, hour, minutes, 0, 0);
-        startDate.setHours( startDate.getHours() - 3);
 
         var appo = {
             id: simpleAppointment._id,
